b35c802808984d8c5842d64bb88aa2f2f1d1d048
Barbara Samsel <bacha.samsel@gmail.com>
2021-06-29 18:55:12 +0200

    First controller in this project.

---
+++ LookAtMe/Controllers/AboutMe.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LookAtMe.Controllers
+{
+    public class AboutMe : Controller
+    {
+        //GET: /AboutMe
+        public string Index()
+        {
+            return "This app will be my portfolio.\nI'm at beggining of my programming journey. Wish me luck!";
+        }
+    }
+}
