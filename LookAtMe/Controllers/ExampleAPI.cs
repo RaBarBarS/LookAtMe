ba62e8451c7a34564aa4689f9a31123adf357929
Barbara Samsel <bacha.samsel@gmail.com>
2021-08-02 22:39:39 +0200

    Added UI draft for example API tab

---
+++ LookAtMe/Controllers/ExampleAPI.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LookAtMe.Controllers
+{
+    public class ExampleAPI : Controller
+    {
+        //GET: /Privacy
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
