4178676eea0ec9c029c1a5328e0e2b5f60ba0919
Barbara Samsel <bacha.samsel@gmail.com>
2021-08-09 22:29:10 +0200

    Added plant model and get method

---
+++ LookAtMe/Models/Plant.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LookAtMe.Models
+{
+    public class Plant
+    {
+        public string Name { get; set; }
+        public string Height { get; set; }
+        public string Summary { get; set; }
+    }
+}
